0b47734f10229a7b570d340700badb54307294f5
AdieDev0 <devutella@gmail.com>
2024-12-21 17:25:26 +0800

    import getAllUsers

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,6 @@
 import User from "../model/userModel.js";
 
+// CREATE
 export const create = async (req, res) => {
   try {
     const { name, email, address } = req.body;
@@ -19,6 +20,20 @@ export const create = async (req, res) => {
     const newUser = new User({ name, email, address });
     const savedData = await newUser.save();
     res.status(200).json(savedData);
+  } catch (error) {
+    res.status(500).json({ errorMessage: error.message });
+  }
+};
+
+// GET ALL USERS
+export const getAllUsers = async (req, res) => {
+  try {
+    const userData = await User.find();
+    if (!userData || userData.length === 0) {
+      return res.status(404).json({ message: "User data not found" });
+    }
+
+    res.status(200).json(userData);
   } catch (error) {
     res.status(500).json({ errorMessage: error.message });
   }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { create } from "../controller/userController.js";
+import { create, getAllUsers } from "../controller/userController.js";
 
 const route = express.Router();
 
+// Define routes
 route.post("/user", create);
+route.get("/users", getAllUsers);
 
 export default route;
