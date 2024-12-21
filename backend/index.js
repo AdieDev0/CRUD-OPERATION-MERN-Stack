251a3160264af5c9ccad7593e10a022afbeff6e1
AdieDev0 <devutella@gmail.com>
2024-12-21 14:47:33 +0800

    Connection DB and Backend

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,21 @@
 import mongoose from "mongoose";
-import mongoose from "mongoose";
+import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+
+const app = express();
+app.use(bodyParser.json());
+dotenv.config();
+
+const PORT = process.env.PORT || 7000;
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose
+  .connect(MONGOURL)
+  .then(() => {
+    console.log("Connected");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
