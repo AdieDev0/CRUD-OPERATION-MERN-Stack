c3a40a9d63d2388f309f2ebfcbde479d0d116374
AdieDev0 <devutella@gmail.com>
2024-12-21 19:26:47 +0800

    UPDATE API

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -52,3 +52,20 @@ export const getUserById = async (req, res) => {
     res.status(500).json({ errorMessage: error.message });
   }
 };
+
+// UPDATE USER
+export const update = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userExist = await User.findById(id);
+    if (!userExist) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const updateData = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(200).json(updateData)
+  } catch (error) {
+    return res.status(404).json({ message: "User not found" });
+  }
+};
