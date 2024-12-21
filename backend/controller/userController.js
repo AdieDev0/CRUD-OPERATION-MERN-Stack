404102298f2dfeb185250f11893852b4ad229a5f
AdieDev0 <devutella@gmail.com>
2024-12-21 18:05:04 +0800

    Something wrong

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -38,3 +38,17 @@ export const getAllUsers = async (req, res) => {
     res.status(500).json({ errorMessage: error.message });
   }
 };
+
+// GET USER WITH SPECIFIC ID
+export const getUserById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userExist = await User.findById(id);
+    if (!userExist) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(userExist);
+  } catch (error) {
+    res.status(500).json({ errorMessage: error.message });
+  }
+};
