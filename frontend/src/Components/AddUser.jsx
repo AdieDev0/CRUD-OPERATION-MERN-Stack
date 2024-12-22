368fa273fbe0fd81124fb2e116844da0583d226e
AdieDev0 <devutella@gmail.com>
2024-12-23 00:57:56 +0800

    Adding style later

--- frontend/src/Components/AddUser.jsx
+++ frontend/src/Components/AddUser.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const AddUser = () => {
   return (
-    <div>AddUser</div>
-  )
-}
+    <div>
+      <div>
+        <h1>Add New User</h1>
+        <form action="">
+          {/* NAME */}
+          <div>
+            <label htmlFor="">Name:</label>
+            <input type="text" placeholder="Enter your Name" required />
+          </div>
+          {/* EMAIL */}
+          <div>
+            <label htmlFor="">Email:</label>
+            <input type="text" placeholder="Enter your Email" required />
+          </div>
+          {/* ADDRESS */}
+          <div>
+            <label htmlFor="">Address:</label>
+            <input type="text" placeholder="Enter your Address" required />
+          </div>
+          {/* BUTTON */}
+          <div>
+            <button>Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default AddUser
+export default AddUser;
